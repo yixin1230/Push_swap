87209f816865eafc21cef06fe90ac03d8d791871
Yixin Zhang <yizhang@f0r1s7.codam.nl>
2023-01-26 11:59:40 +0100

    norm ok

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: yizhang <yizhang@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/24 22:27:56 by yizhang       #+#    #+#                 */
-/*   Updated: 2023/01/26 11:48:46 by yizhang       ########   odam.nl         */
+/*   Updated: 2023/01/26 11:58:49 by yizhang       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,62 +16,27 @@
 static int	flag_check(char *line, t_data *check)
 {
 	if (!ft_strncmp(line, "sa\n", 3))
-	{
-		do_sa(check);
-		return (1);
-	}
+		return (do_sa(check), 1);
 	else if (!ft_strncmp(line, "sb\n", 3))
-	{
-		do_sb(check);
-		return (1);
-	}
+		return (do_sb(check), 1);
 	else if (!ft_strncmp(line, "ss\n", 3))
-	{
-		do_ss(check);
-		return (1);
-	}
+		return (do_ss(check), 1);
 	else if (!ft_strncmp(line, "pa\n", 3))
-	{
-		do_pa(check);
-		return (1);
-	}
+		return (do_pa(check), 1);
 	else if (!ft_strncmp(line, "pb\n", 3))
-	{
-		do_pb(check);
-		return (1);
-	}
+		return (do_pb(check), 1);
 	else if (!ft_strncmp(line, "ra\n", 3))
-	{
-		do_ra(check);
-		return (1);
-	}
+		return (do_ra(check), 1);
 	else if (!ft_strncmp(line, "rb\n", 3))
-	{
-		do_rb(check);
-		return (1);
-	}
+		return (do_rb(check), 1);
 	else if (!ft_strncmp(line, "rr\n", 3))
-	{
-		do_rr(check);
-		return (1);
-	}
+		return (do_rr(check), 1);
 	else if (!ft_strncmp(line, "rrb\n", 4))
-	{
-		do_rrb(check);
-		return (1);
-	}
+		return (do_rrb(check), 1);
 	else if (!ft_strncmp(line, "rra\n", 4))
-	{
-		do_rra(check);
-		return (1);
-	}
+		return (do_rra(check), 1);
 	else if (!ft_strncmp(line, "rrr\n", 4))
-	{
-		do_rrr(check);
-		return (1);
-	}
-	else if (line == NULL)
-		return (1);
+		return (do_rrr(check), 1);
 	else
 		write (1, "Error\n", 6);
 	exit (1);
@@ -98,13 +63,11 @@ int	main(int argc, char **argv)
 	check = create_data(argv, argc);
 	if (argc <= 1 || fill_stack_a(check) == -1)
 		return (0);
-	see_the_stack(&check->a);
 	checker(check);
 	if (is_storted(check) == 1)
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	see_the_stack(&check->a);
 	free_stack_a(check);
 	return (0);
 }
